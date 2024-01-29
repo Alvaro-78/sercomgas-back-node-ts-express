0de90a27446b2c47ca54f232d52342c093a486d9
Alvaro-78 <alvaro78an@gmail.com>
2024-01-29 08:50:13 +0100

    Fixed operations routes

--- src/routes/operationRoutes.ts
+++ src/routes/operationRoutes.ts
@@ -4,9 +4,9 @@ import * as operationController from '../controller/OperationController';
 const router = Router();
 
 router.get('/', operationController.getAllOperations);
-router.get('/:id', operationController.getOperationById);
-router.post('/', operationController.createOperation);
-router.put('/:id', operationController.updateOperation);
-router.delete('/:id', operationController.deleteOperation);
+router.get('/operations/:id', operationController.getOperationById);
+router.post('/operations', operationController.createOperation);
+router.put('/operations/:id', operationController.updateOperation);
+router.delete('/operations/:id', operationController.deleteOperation);
 
 export default router;
