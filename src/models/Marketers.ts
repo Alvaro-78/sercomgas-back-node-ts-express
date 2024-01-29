aab3c9bb4f185815362735d35c5ca10d2cfcbc69
Alvaro-78 <alvaro78an@gmail.com>
2024-01-29 07:05:10 +0100

    Added models

---
+++ src/models/Marketers.ts
@@ -0,0 +1,27 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../db';
+
+export class Marketer extends Model {
+	public id!: number;
+	public name!: string;
+	public readonly createdAt!: Date;
+	public readonly updatedAt!: Date;
+}
+
+Marketer.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		name: {
+			type: new DataTypes.STRING(128),
+			allowNull: false,
+		},
+	},
+	{
+		tableName: 'marketers',
+		sequelize,
+	}
+);
---
+++ src/models/Operations.ts
@@ -0,0 +1,58 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../db';
+import { Marketer } from './Marketers';
+
+export class Operation extends Model {
+	public id!: number;
+	public marketerId!: number;
+	public clientId!: number;
+	public type!: string;
+	public amount!: number;
+	public price!: number;
+	public readonly createdAt!: Date;
+	public readonly updatedAt!: Date;
+}
+
+Operation.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		marketerId: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: Marketer,
+				key: 'id',
+			},
+		},
+		clientId: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: Marketer,
+				key: 'id',
+			},
+		},
+		type: {
+			type: new DataTypes.STRING(128),
+			allowNull: false,
+		},
+		amount: {
+			type: DataTypes.FLOAT,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.FLOAT,
+			allowNull: false,
+		},
+		currenty_currency: {
+			type: new DataTypes.STRING(10),
+			allowNull: false,
+		},
+	},
+	{
+		tableName: 'operations',
+		sequelize,
+	}
+);
