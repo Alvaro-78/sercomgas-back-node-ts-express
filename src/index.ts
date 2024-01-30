3cd860923ccabf86a4256663ee19a43b4b0e1589
Alvaro-78 <alvaro78an@gmail.com>
2024-01-30 16:30:45 +0100

    Added clientRoutes in index

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,17 @@ import cors from 'cors';
 import { sequelize } from './db';
 import marketerRouter from './routes/marketerRoutes';
 import operationRouter from './routes/operationRoutes';
+import clientRouter from './routes/clientRoutes';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(marketerRouter);
 app.use(operationRouter);
+app.use(clientRouter);
 
 app.get('/', (req, res) => {
 	res.send('hola papi');
@@ -19,7 +21,7 @@ app.get('/', (req, res) => {
 
 async function main() {
 	try {
-		await sequelize.sync({ alter: true });
+		await sequelize.sync({ force: true });
 		app.listen(port, () => {
 			console.log(`I am ready in http://localhost:${port}`);
 		});
